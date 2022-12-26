0fac4eb5193066d48d3793994cf3d2ea94cf1df2
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2022-12-26 20:09:02 -0300

    Added reset button

--- src/components/ResetButton.jsx
+++ src/components/ResetButton.jsx
@@ -1,5 +1,10 @@
+import refreshButtonArrows from "../assets/arrows-rotate-solid.svg";
+
 export default () => {
-    return(
-        <button>Reset</button>
-    )
-}
+  return (
+    <button class="refresh-button" onClick="window.location.reload()">
+      <i class="fa-solid fa-arrows-rotate refresh-button-arrows"></i>
+      <p class="refresh-button-text">Reset</p>
+    </button>
+  );
+};
